22990435a46a1bb3933a88cbcfe9aed97b54e877
muthumech <muthumuthuraman97@gmail.com>
2021-12-09 17:28:38 +0530

    task completed

---
+++ src/collegeInformation/Boys.java
@@ -0,0 +1,8 @@
+package collegeInformation;
+
+public class Boys {
+public void boysName() {
+	System.out.println("boys name is muthu");
+
+}
+}
---
+++ src/collegeInformation/Girls.java
@@ -0,0 +1,21 @@
+package collegeInformation;
+
+public class Girls extends Boys {
+private void girlsName() {
+	System.out.println("girls name is sumathi");
+
+}
+public static void main(String[] args) {
+	Girls g = new Girls(); 
+g.boysName();
+g.girlsName();
+
+
+}
+
+
+
+
+
+
+}
